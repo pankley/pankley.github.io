8b7507d692f7bd1a901a7074918c4d4200fa83cb
pankley <38409345+pankley@users.noreply.github.com>
2026-05-19 16:08:26 +0000

    Deploying to gh-pages from @ pankley/pankley.github.io@024dafb1207f3b891fa69b5c0b9e8b2ab37286bf 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -451,8 +451,8 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
-          section: "News",},{id: "projects-project-1",
-          title: 'project 1',
+          section: "News",},{id: "projects-transcriptomics-point-of-departure-uncertainty-estimation",
+          title: 'Transcriptomics Point-of-Departure Uncertainty Estimation',
           description: "with background image",
           section: "Projects",handler: () => {
               window.location.href = "/al-folio/projects/1_project/";
